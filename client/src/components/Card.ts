a6cb0155cb230306dd5e382a66f2e16491172110
romanpoudel <romanpoudel325@gmail.com>
2024-01-09 01:44:40 +0545

    footer

--- client/src/components/Card.ts
+++ client/src/components/Card.ts
@@ -7,14 +7,14 @@ class Card extends HTMLElement {
 							<img src="https://images.unsplash.com/photo-1560185893-a55cbc8c57e8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="house image" />
 						</figure>
 						<div class="card__desc">
-							<h2 class="card__desc--title">Amsterdam</h2>
+							<h2 class="card__desc--title">${this.getAttribute("city")}</h2>
 							<div class="card__desc--info">
 								<div class="card__desc--rating">
 									<i class="bi bi-star-half card__desc--star"></i>
-									  <span class="card__desc--rating-value">4.5</span>
-									  <p class="card__desc--reviews">(93 Reviews)</p>
+									  <span class="card__desc--rating-value">${this.getAttribute("star")}</span>
+									  <p class="card__desc--reviews">(${this.getAttribute("review")} Reviews)</p>
 								</div>
-								<div class="card__desc--price">$156,000USD</div>
+								<div class="card__desc--price">$${this.getAttribute("price")} USD</div>
 							</div>
 						</div>
 					</div>
---
+++ client/src/components/Footer.ts
@@ -0,0 +1,15 @@
+class Footer extends HTMLElement {
+	constructor() {
+		super();
+		this.innerHTML = `
+    <footer class=" footer">
+			<div class="container footer-content">
+				<p>&copy; 2024 Resi-Review. All rights reserved.</p>
+				<p>Contact us: <a href="mailto:romanpoudel325@gmail.com">contactus@gmail.com</a></p>
+			</div>
+		</footer>
+  `;
+	}
+}
+
+customElements.define("footer-component", Footer);
