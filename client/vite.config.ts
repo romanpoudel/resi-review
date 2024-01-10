47c1f7c531b5edbdb6eb56b5bace75c092c8b7a4
romanpoudel <romanpoudel325@gmail.com>
2024-01-10 23:24:36 +0545

    relative path

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,20 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
+import "dotenv/config";
 
-// https://vitejs.dev/config/
+const isGitHubPages =
+	process.env.NODE_ENV === "production" && process.env.GITHUB_PAGES === "true";
+
 export default defineConfig({
-	base: "/resi-review/",
+	base: isGitHubPages ? "/resi-review/" : "/",
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				detail: resolve(__dirname, "src/views/details/index.html"),
+				login: resolve(__dirname, "src/views/login/index.html"),
+				nested: resolve(__dirname, "src/views/signup/index.html"),
+			},
+		},
+	},
 });
