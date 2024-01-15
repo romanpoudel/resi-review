c826af462720af212a457f1779a7f46aa71def76
romanpoudel <romanpoudel325@gmail.com>
2024-01-15 22:02:31 +0545

    image upload from frontend

--- client/src/views/profile/script.ts
+++ client/src/views/profile/script.ts
@@ -17,3 +17,33 @@ logoutBtn.addEventListener("click", async () => {
 		console.log(err.response.data.message);
 	}
 });
+
+const profileImage = document.getElementById(
+	"profile-image"
+) as HTMLInputElement;
+const form = document.getElementById("profile-form") as HTMLFormElement;
+
+form.addEventListener("submit", async (e) => {
+	e.preventDefault();
+	const formData = new FormData();
+	formData.append("profileimage", profileImage?.files?.[0] || "");
+	const requestOptions = {
+		method: "POST",
+		url: "/users/update-profile",
+		data: formData,
+		headers: {
+			"Content-Type": "multipart/form-data",
+		},
+	};
+
+	try{
+		const response = await api(requestOptions);
+		console.log(response);
+		if (response.status === 201) {
+			window.location.reload();
+		}
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	}catch(err:any){
+		console.log(err.response.data.message);
+	}
+});
